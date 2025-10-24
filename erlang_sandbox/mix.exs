74881d2db5b9548c3563193bd8293bdfcc3acc50
Ms0r <themasor07@gmail.com>
2025-10-24 14:18:26 +0000

    feat:Added CI workflow

--- erlang_sandbox/mix.exs
+++ erlang_sandbox/mix.exs
@@ -15,14 +15,12 @@ defmodule ErlangSandbox.MixProject do
           applications: [runtime_tools: :permanent]
         ]
       ],
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
-      ]
+      test_coverage: [tool: ExCoveralls]
     ]
+  end
+
+  def cli do
+    [preferred_envs: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]]
   end
 
   # Run "mix help compile.app" to learn about applications.
