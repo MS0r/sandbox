900d9f3345dc8ace92d13451766e771c29d912fc
Mickael <themasor07@gmail.com>
2026-02-06 23:02:17 +0000

    fix environment variables

--- erlang_sandbox/config/runtime.exs
+++ erlang_sandbox/config/runtime.exs
@@ -1,19 +1,22 @@
 import Config
 import Dotenvy
 
-env_dir_prefix = System.get_env("RELEASE_ROOT") || Path.expand("./")
+if config_env() != :test do
+  env_dir_prefix = System.get_env("RELEASE_ROOT") || Path.expand("./")
 
-source!([
-  Path.absname(".env", env_dir_prefix),
-  Path.absname("../.env",env_dir_prefix),
-  Path.absname(".#{config_env()}.env", env_dir_prefix),
-  Path.absname(".#{config_env()}.overrides.env", env_dir_prefix),
-  System.get_env()
-])
+  source!([
+    Path.absname(".env", env_dir_prefix),
+    Path.absname("../.env",env_dir_prefix),
+    Path.absname(".#{config_env()}.env", env_dir_prefix),
+    Path.absname(".#{config_env()}.overrides.env", env_dir_prefix),
+    System.get_env()
+  ])
 
-config :erlang_sandbox,
+
+  config :erlang_sandbox,
     env: config_env(),
     port: env!("RABBITMQ_PORT", :number!, 5672),
     host: env!("RABBITMQ_HOST", :string!),
     user: env!("RABBITMQ_DEFAULT_USER", :string),
     password: env!("RABBITMQ_DEFAULT_PASS", :string)
+end
--- erlang_sandbox/config/test.exs
+++ erlang_sandbox/config/test.exs
@@ -1,3 +1,10 @@
 import Config
 
 config :logger, level: :error
+
+config :erlang_sandbox,
+  env: config_env(),
+  port: 0,
+  host: "localhost",
+  user: "user",
+  password: "password"
